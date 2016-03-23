de68325f8bee4d64fd1ac9d554b6e5ca4a6f53e3
Tim Licata <tim@timothylicata.com>
2016-03-22 17:04:55 -0700

    install ruby 2.2.4

---
+++ install/ruby.sh
@@ -0,0 +1,9 @@
+# Install versions of Ruby using rbenv.
+
+# I had a tough time getting a shell that had rbenv available.
+# Followed some of the suggestions here:
+# http://stackoverflow.com/questions/29132891/vagrant-virtualbox-vm-provisioning-rbenv-installs-successfully-but-subsequent-u
+
+sudo -H -u vagrant bash -i -c 'rbenv install --skip-existing 2.2.4'
+sudo -H -u vagrant bash -i -c 'rbenv global 2.2.4'
+sudo -H -u vagrant bash -i -c 'rbenv rehash'
