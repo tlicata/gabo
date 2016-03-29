f865801778b4889bb61b9972fadeb8d57a1230ca
Tim Licata <tim@timothylicata.com>
2016-03-28 21:15:09 -0400

    install nvm

---
+++ install/nvm.sh
@@ -0,0 +1,4 @@
+# Install nvm (Node Version Manager) via the nvm install script:
+# https://github.com/creationix/nvm#install-script
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
