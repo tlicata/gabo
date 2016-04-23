ab2a48b757003a64f7fdbf0de374c03d4f0f9575
Tim Licata <tim@timothylicata.com>
2016-04-22 18:48:15 -0700

    change default ruby version to 2.3.0

--- install/ruby.sh
+++ install/ruby.sh
@@ -4,6 +4,6 @@
 # Followed some of the suggestions here:
 # http://stackoverflow.com/questions/29132891/vagrant-virtualbox-vm-provisioning-rbenv-installs-successfully-but-subsequent-u
 
-sudo -H -u vagrant bash -i -c 'rbenv install --skip-existing 2.2.4'
-sudo -H -u vagrant bash -i -c 'rbenv global 2.2.4'
+sudo -H -u vagrant bash -i -c 'rbenv install --skip-existing 2.3.0'
+sudo -H -u vagrant bash -i -c 'rbenv global 2.3.0'
 sudo -H -u vagrant bash -i -c 'rbenv rehash'
