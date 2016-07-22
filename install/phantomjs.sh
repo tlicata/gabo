c3cf1a13ff6a4efef91b69d50074125948275119
Tim Licata <tim@timothylicata.com>
2016-07-22 15:17:06 -0700

    upgrade phantomjs from 1.9.8 to 2.1.1

--- install/phantomjs.sh
+++ install/phantomjs.sh
@@ -9,9 +9,9 @@ BASHRC=/home/vagrant/.bashrc
 
 if ! grep "$PHANTOM_COMMENT" $BASHRC > /dev/null; then
     cd /opt/
-    wget -q https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2
-    tar xjf phantomjs-1.9.8-linux-x86_64.tar.bz2
-    cd phantomjs-1.9.8-linux-x86_64
+    wget -q https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+    tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
+    cd phantomjs-2.1.1-linux-x86_64
     echo "" >> $BASHRC
     echo "$PHANTOM_COMMENT" >> $BASHRC
     echo "export PATH=\"\$PATH:$(pwd)/bin\"" >> $BASHRC
