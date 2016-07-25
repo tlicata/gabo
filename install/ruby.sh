b111fa1cf6a77c395c93e88246ecda3f6dbec7d6
Tim Licata <tim@timothylicata.com>
2016-07-25 10:27:18 -0700

    install ruby 2.3.1

--- install/ruby.sh
+++ install/ruby.sh
@@ -4,6 +4,8 @@
 # Followed some of the suggestions here:
 # http://stackoverflow.com/questions/29132891/vagrant-virtualbox-vm-provisioning-rbenv-installs-successfully-but-subsequent-u
 
-sudo -H -u vagrant bash -i -c 'rbenv install --skip-existing 2.3.0'
+# sudo -H -u vagrant bash -i -c 'rbenv install --skip-existing 2.1.2'
+# sudo -H -u vagrant bash -i -c 'rbenv install --skip-existing 2.3.0'
+sudo -H -u vagrant bash -i -c 'rbenv install --skip-existing 2.3.1'
 sudo -H -u vagrant bash -i -c 'rbenv global 2.3.0'
 sudo -H -u vagrant bash -i -c 'rbenv rehash'
