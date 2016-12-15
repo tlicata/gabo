f0eefe67d0e6b8885f2d709e272815a849853b2d
Tim Licata <tim@timothylicata.com>
2016-12-14 20:59:08 -0500

    change postgres script to ubuntu user

--- install/postgres.sh
+++ install/postgres.sh
@@ -3,10 +3,10 @@
 # Install Postgres.
 sudo apt-get --yes install postgresql postgresql-contrib libpq-dev
 
-# Create a user named `vagrant`.
+# Create a user named `ubuntu`.
 # To figure out if user already exists I followed:
 # http://stackoverflow.com/questions/8546759/how-to-check-if-a-postgres-user-exists
-ROLE_EXISTS=$(sudo -i -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='vagrant'")
+ROLE_EXISTS=$(sudo -i -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='ubuntu'")
 if [ -z "$ROLE_EXISTS" ]; then
-    sudo --user postgres createuser --superuser vagrant
+    sudo --user postgres createuser --superuser ubuntu
 fi
