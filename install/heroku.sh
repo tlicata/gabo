087a2e2505c5ed9776b32f58884f59f54b17ff77
Tim Licata <tim@timothylicata.com>
2018-04-11 15:22:13 -0400

    update heroku install url

--- install/heroku.sh
+++ install/heroku.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if ! command -v heroku >/dev/null 2>&1; then
-    wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+    wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 fi
